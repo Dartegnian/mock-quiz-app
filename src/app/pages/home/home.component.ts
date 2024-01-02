26d8a9a6f8fcf8f9fa43d800fc1109fdf59c69e9
Dartegnian <root@dartegnian.com>
2024-01-02 19:18:15 +0800

    Write basic home page and hero banner

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeroBannerComponent } from '@components/hero-banner/hero-banner.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+	HeroBannerComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
