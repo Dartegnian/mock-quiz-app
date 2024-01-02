782a7ea44b6963eea33de4cea7a83039cce64363
Dartegnian <root@dartegnian.com>
2024-01-02 22:12:47 +0800

    Allow question block to receive input/props from parent component, display in DOM

--- src/app/components/question-block/question-block.component.ts
+++ src/app/components/question-block/question-block.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import IQuizQuestion from '@interfaces/quiz-question.interface';
 
 @Component({
-  selector: 'app-question-block',
-  standalone: true,
-  imports: [],
-  templateUrl: './question-block.component.html',
-  styleUrl: './question-block.component.scss'
+	selector: 'app-question-block',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './question-block.component.html',
+	styleUrl: './question-block.component.scss'
 })
 export class QuestionBlockComponent {
-
+	@Input() question: IQuizQuestion = {
+		type: "multiple-choice",
+		question: "",
+		answer: "",
+		pointGrade: 0,
+		choices: [
+			""
+		]
+	};
 }
