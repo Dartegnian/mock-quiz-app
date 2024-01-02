9a6a52f66310a44620466c16c7dd300147c4a4d4
Dartegnian <root@dartegnian.com>
2024-01-02 21:52:14 +0800

    Write basic question block with no data

---
+++ src/app/components/question-block/question-block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './question-block.component.html',
+  styleUrl: './question-block.component.scss'
+})
+export class QuestionBlockComponent {
+
+}
