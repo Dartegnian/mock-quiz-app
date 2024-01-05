6c95252c97f3213f34ac0be930c3fd489fc750d2
Dartegnian <root@dartegnian.com>
2024-01-06 03:09:53 +0800

    Add 3rd image, adjust text highlight, use variables

--- src/app/components/hero-banner/hero-banner.component.ts
+++ src/app/components/hero-banner/hero-banner.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 	styleUrl: './hero-banner.component.scss'
 })
 export class HeroBannerComponent {
+	taglineText = "See Your Students' Potential,";
+	highlightedText = "Through Exams.";
+	ctaText = "Embark on a transformative learning experience with our user-friendly exam platform. Unlock your students' potential and pave the way for academic success.";
 }
