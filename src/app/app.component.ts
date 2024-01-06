0e7b2076d7bcd066582d756690134642e3671708
Dartegnian Velarde <root@dartegnian.com>
2024-01-07 04:32:46 +0800

    Show login form on app DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,28 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '@components/nav-bar/nav-bar.component';
+import { LoginModalService } from '@services/login-modal.service';
+import { LoginModalComponent } from '@components/login-modal/login-modal.component';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet, NavBarComponent, LoginModalComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mock-quiz-app';
+	title = 'mock-quiz-app';
+	showModalSubscription: Subscription;
+	isLoginModalShown = false;
+
+	constructor(
+		private loginModalService: LoginModalService
+	) {
+		this.isLoginModalShown = this.loginModalService.isLoginModalShown;
+		this.showModalSubscription = this.loginModalService.showModalSubscription.subscribe(
+			(isShown) => this.isLoginModalShown = isShown
+		);
+	}
 }
